30e1a3ee80065e163b1114af811301ac557ebaf9
Thinking-X <421623216@qq.com>
2018-10-17 21:07:07 +0800

    Algorithm

---
+++ 出现次数最多的整数.cpp
@@ -0,0 +1,67 @@
+/*
+问题描述
+　　编写一个程序，读入一组整数，这组整数是按照从小到大的顺序排列的，它们的个数N也是由用户输入的，最多不会超过20。然后程序将对这个数组进行统计，把出现次数最多的那个数组元素值打印出来。如果有两个元素值出现的次数相同，即并列第一，那么只打印比较小的那个值。
+　　输入格式：第一行是一个整数N(N <= 20)；接下来有N行，每一行表示一个整数，并且按照从小到大的顺序排列。
+　　输出格式：输出只有一行，即出现次数最多的那个元素值。
+输入输出样例
+样例输入
+5
+100
+150
+150
+200
+250
+样例输出
+150
+*/ 
+#include<iostream>
+#include<Vector>
+using namespace std;
+
+int main()
+{
+	int N,n_temp;
+	cin >> N;
+	if (N > 0&&N<=20)
+	{
+		n_temp = N;
+		vector<int>a = vector<int>();
+		vector<int>b = vector<int>();
+		int Count = 0;
+		while (n_temp > 0)
+		{
+			int temp;
+			cin >> temp;
+			if (n_temp == N)
+			{
+				b.push_back(temp);
+			}
+			if (temp == b[b.size() - 1])
+			{
+				Count++;
+			}
+			else
+			{
+				a.push_back(Count);
+				b.push_back(temp);
+				Count = 1;
+			}
+			n_temp--;
+		}
+		a.push_back(Count);
+		int flag_a = 0, flag_b;
+		int most_Index = 0;
+		for (int i = 0; i < b.size(); i++)
+		{
+			if (a[i] > flag_a)
+			{
+				flag_a = a[i];
+				most_Index = i;
+			}
+		}
+		cout << b[most_Index] << endl;
+	}
+	getchar();
+	getchar();
+    return 0;
+}
---
+++ 队列操作.cpp
@@ -0,0 +1,68 @@
+/*
+问题描述
+　　根据输入的操作命令，操作队列（1）入队、（2）出队并输出、（3）计算队中元素个数并输出。
+输入格式
+　　第一行一个数字N。
+　　下面N行，每行第一个数字为操作命令（1）入队、（2）出队并输出、（3）计算队中元素个数并输出。
+输出格式
+　　若干行每行显示一个2或3命令的输出结果。注意：2.出队命令可能会出现空队出队（下溢），请输出“no”，并退出。
+样例输入
+7
+1 19
+1 56
+2
+3
+2
+3
+2
+样例输出
+19
+1
+56
+0
+no
+数据规模和约定
+　　1<=N<=50
+*/ 
+#include<iostream>
+#include<Queue>
+using namespace std;
+
+
+int main()
+{
+	int N = 0;
+	cin >> N;
+	queue<int> a = queue<int>();
+	int temp1,temp2;
+	while (N > 0)
+	{
+		cin >> temp1;
+		if (temp1 == 1)
+		{
+			cin >> temp2;
+			a.push(temp2);
+		}
+		else if (temp1 == 2)
+		{
+			if (a.size() == 0)
+			{
+				cout << "no" << endl;
+				return 0;
+			}
+			else
+			{
+				cout << a.front() << endl;
+				a.pop();
+			}
+		}
+		else
+		{
+			cout << a.size() << endl;
+		}
+		N--;
+	}
+	getchar();
+	getchar();
+    return 0;
+}
